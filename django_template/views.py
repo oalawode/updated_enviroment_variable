b98c08a233bbd85f93d38c99be73fd607ff83093
Sunday Abam <60232994+SundayAbamjnr@users.noreply.github.com>
2021-04-22 22:50:17 +0100

    Update views.py

--- django_template/views.py
+++ django_template/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-def home_view(request):
-    return render(request,"template/home.html"
+def home_view(request,id =1):
+    return render(request,"home.html"
     )
